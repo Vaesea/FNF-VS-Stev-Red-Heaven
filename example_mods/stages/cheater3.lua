7ded1c1cd18d06863bd3286e665f687f7009fadb
EggsAndBakey <159406785+EggsAndBakey@users.noreply.github.com>
2024-05-21 22:34:04 +0100

    added songs, stages and some almost-empty folders.
    bill.
    FUTURE THINGS COULD CHANGE AT ANY TIME AS THOSE SONGS WILL, AT SOME POINT, BE ADDED TO THE MOD OR SCRAPPED FROM THE MOD, DEPENDING ON WHAT HAPPENS AND WHAT CHANGES.

---
+++ example_mods/stages/cheater3.lua
@@ -0,0 +1,6 @@
+function onCreate()
+    makeLuaSprite('bg', 'stageimages/evilstevtwo', -600, -400)
+    addLuaSprite('bg', false)
+    addGlitchEffect('bg',1,5,0.9,100)
+    setScrollFactor('bg', 0, 0)
+end
---
+++ example_mods/stages/readME.txt
@@ -0,0 +1,3 @@
+This is where your stage .jsons and .luas go.
+You do not need a lua to create a stage, as the json can include information for sprites and animations.
+But I do NOT care????
