f1b1aec5c34181573f5c6e036a0483d9a780c1b3
EggsAndBakey <159406785+EggsAndBakey@users.noreply.github.com>
2024-06-30 21:57:59 +0100

    Update build_x64-debug.bat

--- art/build_x64-debug.bat
+++ art/build_x64-debug.bat
@@ -1,7 +1,7 @@
 @echo off
 color 0a
 cd ..
-echo BUILDING VS STEV RED HEAVEN WINDOWS x64
+echo BUILDING VS STEV RED HEAVEN WINDOWS x64 DEBUG
 haxelib run lime build windows -debug
 echo.
 echo Done.
