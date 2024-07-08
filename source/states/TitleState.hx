9ee5ca2620627112e926fce641f596c713deda8b
EggsAndBakey <159406785+EggsAndBakey@users.noreply.github.com>
2024-07-08 14:22:44 +0100

    Demo 1.5 Beta 2
    * edited source code things (NEEDS TO BE TESTED)
    
    i dont test before i commit the source code stuff because i'm dumb.
    
    * yeah (NEEDS TO BE TESTED)
    
    * remove character editor option, NEEDS TO BE TESTED
    
    character editor didn't work in vsrh anyways
    
    * Update gitVersion.txt
    
    * Delete .github/ISSUE_TEMPLATE directory
    
    * Delete docs/CNAME
    
    what even is this?
    
    * Delete docs directory
    
    bye bye!
    
    * Update LICENSE
    
    is this illegal
    
    * Update .gitattributes
    
    * Update hxformat.json
    
    * Unbroke it.

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -549,16 +549,16 @@ class TitleState extends MusicBeatState
 					FlxG.sound.music.fadeIn(4, 0, 0.7);
 				case 2:
 					#if PSYCH_WATERMARKS
-					createCoolText(['VS Stev Red Heaven by'], 40);
+					createCoolText(['VS Stev Red Heaven'], 40);
 					#else
 					createCoolText(['EggsAndBakey']);
 					#end
 				case 4:
 					#if PSYCH_WATERMARKS
-					addMoreText('a', 40);
-					addMoreText('b', 40);
+					addMoreText('The Mod', 40);
+					addMoreText('Ever', 40);
 					#else
-					addMoreText('present');
+					addMoreText('yea');
 					#end
 				case 5:
 					deleteCoolText();
--- source/states/editors/MasterEditorMenu.hx
+++ source/states/editors/MasterEditorMenu.hx
@@ -11,7 +11,6 @@ class MasterEditorMenu extends MusicBeatState
 {
 	var options:Array<String> = [
 		'Chart Editor',
-		'Character Editor',
 		'Week Editor',
 		'Menu Character Editor',
 		'Dialogue Editor',
@@ -106,8 +105,6 @@ class MasterEditorMenu extends MusicBeatState
 			switch(options[curSelected]) {
 				case 'Chart Editor'://felt it would be cool maybe
 					LoadingState.loadAndSwitchState(new ChartingState(), false);
-				case 'Character Editor':
-					LoadingState.loadAndSwitchState(new CharacterEditorState(Character.DEFAULT_CHARACTER, false));
 				case 'Week Editor':
 					MusicBeatState.switchState(new WeekEditorState());
 				case 'Menu Character Editor':
